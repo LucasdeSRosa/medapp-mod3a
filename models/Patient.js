b91bd2d70a8b4b7f2de330845e5d439c5eecc187
Lucas Rosa <rosa.lucas.contato@gmail.com>
2025-02-19 20:22:53 -0300

    criacao de modelos para o banco nao relacional

---
+++ models/Patient.js
@@ -0,0 +1,31 @@
+import { mongoose } from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const patientSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Patient name is required'],
+  },
+  birthDate: {
+    type: Date,
+    required: [true, 'Birth date is required'],
+    },  
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone is required'],
+  },
+}
+);
+
+const patient = mongoose.model('patient', patientSchema);
+export default patient;
